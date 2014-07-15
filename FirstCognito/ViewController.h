c27125117c5cab9a0ac42eb41faa939024b9c409
kawatomo <kawatomo@city.co.jp>
2014-07-15 13:21:16 +0900

    Initial Commit

---
+++ FirstCognito/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FirstCognito
+//
+//  Created by 河野 智遵 on 2014/07/15.
+//  Copyright (c) 2014年 Jabaraster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
